e2dba24c317f491c8b55e0acbb279a445c1645d2
davidiscool <dammyadavid@gmail.com>
2023-08-12 00:30:39 +0100

    CREATED the bose website in vue JS, *NO RESPONSIVENESS

---
+++ src/App.vue
@@ -0,0 +1,205 @@
+<script setup></script>
+
+<template>
+  <div class="navbar">
+    <img src="./assets/logo.svg" alt="bose-logo" />
+
+    <div class="navbar-right">
+      <button>
+        <p>NEW ARRIVALS</p>
+      </button>
+
+      <ul>
+        <li>HeadPhones</li>
+        <li>Speakers</li>
+        <li>Support</li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="hero">
+    <div class="hero-left">
+      <h2>Light. Sound. Celebrate.</h2>
+      <h1>This year, bring home.</h1>
+      <button>SHOP</button>
+    </div>
+
+    <img src="./assets/bar.svg" alt="" />
+  </div>
+
+  <div class="comfort">
+    <div class="comfort-left">
+      <p class="head">
+        QuietComfort <br />
+        35 wireless headphones II
+      </p>
+      <p class="select">Select your color:</p>
+      <img src="./assets/colors.svg" alt="colors" />
+      <h4>Silver</h4>
+      <div class="comfort-left-inner">
+        <p>$229</p>
+        <button>BUY NOW</button>
+      </div>
+    </div>
+    <div class="comfort-right">
+      <img class="whitee" src="./assets/white.svg" alt="" />
+      <img class="whitee" src="./assets/union.svg" alt="" />
+      <img class="set" src="./assets/bose1.svg" alt="" />
+    </div>
+  </div>
+
+  <div class="featured">
+    <h1>FEATURED PRODUCTS</h1>
+
+    <ul>
+      <li><img src="./assets/left.svg" alt="" /></li>
+      <li><img src="./assets/fp1.svg" alt="" /></li>
+      <li><img src="./assets/fp2.svg" alt="" /></li>
+      <li><img src="./assets/fp3.svg" alt="" /></li>
+      <li><img src="./assets/fp4.svg" alt="" /></li>
+      <li><img src="./assets/fp5.svg" alt="" /></li>
+      <li><img src="./assets/right.svg" alt="" /></li>
+    </ul>
+  </div>
+
+  <div class="sound">
+    <div class="sound-left">
+      <h2>Astonishing sound. Wherever life takes you.</h2>
+      <h1>SOUNDLINK FLEX BLUETOOTH® SPEAKER</h1>
+      <button>LEARN MORE</button>
+    </div>
+
+    <img src="./assets/fake.svg" alt="" />
+  </div>
+
+  <div class="premium">
+    <h1>Premium Design</h1>
+    <h3>
+      When you want to take your music anywhere, you need headphones built to
+      keep <br />
+      up. With impact-resistant materials, glass-filled nylon, and
+      corrosion-resistant <br />
+      stainless steel, they’re engineered to survive life on the go.
+    </h3>
+
+    <div class="bottom-p">
+      <img class="uni" src="./assets/unionl.svg" alt="" />
+      <img class="uni" src="./assets/unionr.svg" alt="" />
+      <img class="big" src="./assets/big.svg" alt="" />
+    </div>
+
+    <div class="extra">
+      <img src="./assets/plus.svg" alt="" />
+      <img src="./assets/impact.svg" alt="" />
+    </div>
+  </div>
+
+  <div class="battery">
+    <div class="battery-left">
+      <h1>Up to 20 hours battery life</h1>
+      <h3>
+        Enjoy up to 20 hours of listening, enough time to pore <br />
+        through this entire playlist. So you can keep the music — or <br />the
+        quiet — going all day long. And when it finally runs down,<br />
+        a quick 15-minute charge gives you another 2.5 hours.
+      </h3>
+    </div>
+
+    <img src="./assets/battery.svg" alt="" />
+  </div>
+
+  <div class="related">
+    <div class="top">
+      <h1>RELATED PRODUCTS</h1>
+      <p>SHOW ALL</p>
+    </div>
+
+    <ul>
+      <li>
+        <div class="ima">
+          <img src="./assets/rp1.svg" alt="" />
+        </div>
+        <h2>QuietComfort 35 <br />wireless <br />headphones II</h2>
+        <p class="real">$449.99</p>
+      </li>
+      <li>
+        <div class="ima">
+          <img src="./assets/rp2.svg" alt="" />
+        </div>
+        <h2>Soundlink <br />aroundear wireless <br />headphones</h2>
+        <p class="real">$269.99</p>
+      </li>
+      <li>
+        <div class="ima">
+          <img src="./assets/rp3.svg" alt="" />
+        </div>
+        <h2>Bose on-ear <br />wireless <br />headphones</h2>
+        <p class="real">$209.99</p>
+      </li>
+      <li>
+        <div class="ima">
+          <img src="./assets/rp4.svg" alt="" />
+        </div>
+        <h2>Bose Noise <br />Cancelling <br />headphones 700</h2>
+        <p class="real">coming Soon</p>
+      </li>
+    </ul>
+  </div>
+
+  <div class="bose">
+    <img src="./assets/biglogo.svg" alt="" />
+    <p>
+      A message to Bose Professional customers: An important loudspeaker recall
+      notice.
+    </p>
+    <button>LEARN MORE</button>
+  </div>
+
+  <div class="social">
+    <div class="media">
+      <img src="./assets/facebook.svg" alt="" />
+      <img src="./assets/youtube.svg" alt="" />
+    </div>
+
+    <div class="mail">
+      <img src="./assets/mail.svg" alt="" />
+      <p>Email sign-up</p>
+    </div>
+  </div>
+
+  <div class="footer">
+    <div class="footer-top">
+      <h1>
+        BOSE PROFESSIONAL <br />
+        AUTOMOTIVE SYSTEMS <br />
+        BOSE WORK
+      </h1>
+
+      <div class="footer-tr">
+        <ul>
+          <li>Support</li>
+          <li>Contact Us</li>
+          <li>Find a Store</li>
+          <li>About Us</li>
+          <li>Work at Bose</li>
+          <li>Bose incentives, Awards & Gifts</li>
+          <li>Bose Worldwide</li>
+        </ul>
+        <ul>
+          <li>Privacy policy</li>
+          <li>Terms of Use</li>
+          <li>SMS Terms of use</li>
+          <li>Beware of counterfeits</li>
+          <li>Cookies notice</li>
+          <li>Copyright Policy</li>
+        </ul>
+      </div>
+    </div>
+    <div class="dash"></div>
+    <div class="foot">
+      <img src="./assets/logo.svg" alt="">
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
